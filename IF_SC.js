394f613b2da5912049b8eb58c53035fd34470a04
Caio R. S. de Araújo <159500703+silvaraujo@users.noreply.github.com>
2024-04-17 21:49:18 -0300

    Update IF_SC.js

--- IF_SC.js
+++ IF_SC.js
@@ -1,166 +1,177 @@
-//Começamos a resolução a partir da Aba "Seus Clientes"
+setTimeout(() => {
+    const crieNovasPromos_button = document.querySelector("#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe > div.indexstyles__PageSectionWrapper-partner-portal-crm__sc-1xv6gqj-0.hQKZRA > div.sc-gGnURB.iJWWCJ > div.sc-crXcEl.jwSbcz > div > div > button");
+    crieNovasPromos_button.click();
+}, 5000);
 
-//Aperta do primeiro botão
-const crieNovasPromo_button = document.querySelector("#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe > div.indexstyles__PageSectionWrapper-partner-portal-crm__sc-1xv6gqj-0.hQKZRA > div.sc-gGnURB.iJWWCJ > div.sc-crXcEl.jwSbcz > div > div > button");
-crieNovasPromo_button.click();
+setTimeout(() => {
+    const spam_primeiro = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.jRpMA-d > div:nth-child(1)');
+    spam_primeiro.click();
+}, 10000);
 
-//Clica na aba seguidores  
-const seguidores_option = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.jRpMA-d > div:nth-child(1) > div.sc-bczRLJ.hbUVXt > span');
-seguidores_option.click();
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 15000);
 
-//Aperta o botão continuar. Aparece várias vezes no código
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+setTimeout(() => {
+    const itensPorPag_list = document.querySelector('[id^="downshift-"][id$="-toggle-button"]');   
+    itensPorPag_list.click(); 
+}, 19000);
 
-//Aperta o listbox. !! parece mudar de endereço dentro de "-n-"
-const list_open = document.querySelector('#downshift-0-toggle-button');
-list_open.click();
+setTimeout(() => {
+    const list100_select = document.querySelector('[id^="downshift-"][id$="-item-4"]');
+    list100_select.click();
+}, 21000);
 
-//Aperta o a opção 100 no listbox !! mesma situação do código anterior
-const list100_select = document.querySelector('#downshift-94-item-4');
-list100_select.click();
+setTimeout(() => {
+    const select_all = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-duzrYq.jtmVIG > table > thead > tr > th:nth-child(1) > div.sc-fLlhyt.bdchqW > div > div > span');
+    select_all.click();
+}, 25000);
 
-//Seleciona todos os clientes da tela
-const Span_Select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-duzrYq.jtmVIG > table > thead > tr > th:nth-child(1) > div.sc-fLlhyt.bdchqW > div > div > span');
-Span_Select.click();
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 30000);
 
-//Seleciona o botão continuar, novamente
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+setTimeout(() => {
+    const entrega_select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.iTOJzk > div:nth-child(1) > div.sc-bczRLJ.kJSGHS');
+    entrega_select.click();
+}, 35000);
 
-//Seleciona entrega grátis
-const entrega_select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.iTOJzk > div:nth-child(1) > div.sc-bczRLJ.kJSGHS');
-entrega_select.click();
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 40000);
 
-//Seleciona o botão continuar, novamente
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+setTimeout(() => {
+    const date_box = document.querySelector('#dueDate');
+    const selecionar_data = () => {
+        const hoje = new Date();
+        const selector_quatrosemanas = document.querySelectorAll('.DayPicker-Week .DayPicker-inner-day');
 
-//Clica no input data
-var clica_list = document.querySelector('#dueDate');
-clica_list.click();
+        for (const data of selector_quatrosemanas) {
+            const numeroDia = parseInt(data.textContent, 10);
 
-//Seleciona a data do dia de amanhã
-// Obter a data de hoje
-const hoje = new Date();
+            if (numeroDia > hoje.getDate()) {
+                data.click();
+                break;
+            }
+        }
+    };
+    date_box.click();
+    selecionar_data();
+}, 45000);
 
-// Selecionar todos os elementos dentro de DayPicker-Week
-const selector_quatrosemanas = document.querySelectorAll('.DayPicker-Week .DayPicker-inner-day');
+setTimeout(() => {
+    const continuar2_button = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 50000);
 
-// Iterar sobre os elementos para encontrar o primeiro número maior que o dia de hoje
-for (const data of selector_quatrosemanas) {
-    // Obter o número do dia do elemento e converter para número
-    const numeroDia = parseInt(data.textContent, 10);
+setTimeout(() => {
+    const gratisentrega_select = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-bZkfAO.eJQDwa > div > div > div:nth-child(4) > label');
+    gratisentrega_select.click();
+}, 55000);
 
-    // Verificar se o número do dia é maior que o dia de hoje
-    if (numeroDia > hoje.getDate()) {
-        // Simular o clique no elemento
-        data.click();
-        // Parar a iteração após o clique
-        break;
-    }
-}
+setTimeout(() => {
+    const continuar2_button = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 60000);
 
-//Seleciona o botão continuar após a última ação
-const continuar2_button = document.querySelector('body > div:nth-child(31) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
+setTimeout(() => {
+    const continuar2_button = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 65000);
 
-//Seleciona a opção - entrega grátis para quem segue
-const final1_select = document.querySelector('body > div:nth-child(31) > div.sc-llJcti.eqJNn > div > div.sc-bZkfAO.eJQDwa > div > div > div:nth-child(4) > label');
-final1_select.click();
+setTimeout(() => {
+    const ultimatela = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-hKMtZM.eIDMHW.sc-ezWOiH.janTdf');
+    ultimatela.click();
+}, 70000);
 
-//Seleciona o botão continuar após a seleção, novamente
-const continuar2_button = document.querySelector('body > div:nth-child(31) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
+setTimeout(() => {
+    const voltar_inicio = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-1fkx9ru-0.jsPyvf > button');
+    voltar_inicio.click();
+}, 75000);
 
-//Seleciona o botão continuar após a última verificação, com o mesmo endereço
-const continuar2_button = document.querySelector('body > div:nth-child(31) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
+setTimeout(() => {
+    const crieNovasPromos_button = document.querySelector("#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe > div.indexstyles__PageSectionWrapper-partner-portal-crm__sc-1xv6gqj-0.hQKZRA > div.sc-gGnURB.iJWWCJ > div.sc-crXcEl.jwSbcz > div > div > button");
+    crieNovasPromos_button.click();
+}, 80000);
 
-//fechar a última tela
-//Seleciona o botão continuar após a última ação
-const ultimatela_close = document.querySelector('body > div:nth-child(31) > div.sc-llJcti.eqJNn > div > div.sc-hKMtZM.eIDMHW.sc-ezWOiH.janTdf');
-ultimatela_close.click();
+setTimeout(() => {
+    const spam_segundo = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.jRpMA-d > div:nth-child(2)');
+    spam_segundo.click();
+}, 85000);
 
-//Começamos a resolução a partir da tela final do último código"
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 90000);
 
-//Seleciona o botão continuar após a última verificação, com o mesmo endereço
-const voltar_begin = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-1fkx9ru-0.jsPyvf > button');
-voltar_begin.click();
+setTimeout(() => {
+    const itensPorPag_list = document.querySelector('[id^="downshift-"][id$="-toggle-button"]');   
+    itensPorPag_list.click(); 
+}, 94000);
 
-//Aperta do primeiro botão
-const crieNovasPromo_button = document.querySelector("#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe > div.indexstyles__PageSectionWrapper-partner-portal-crm__sc-1xv6gqj-0.hQKZRA > div.sc-gGnURB.iJWWCJ > div.sc-crXcEl.jwSbcz > div > div > button");
-crieNovasPromo_button.click();
+setTimeout(() => {
+    const list100_select = document.querySelector('[id^="downshift-"][id$="-item-4"]');
+    list100_select.click();
+}, 96000);
 
-//Clica na aba quem adora comprar na loja !! observar o seletor desse código e comparar com o primeiro select. Ele aparecentemente funciona com 2 clicks
-const opção2_select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.jRpMA-d > div.sc-crXcEl.iKLSdb.indexstyles__CardWrapper-partner-portal-crm__sc-1rtmpyq-1.kdGjDu');
-opção2_select.click();
+setTimeout(() => {
+    const select_all = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-duzrYq.jtmVIG > table > thead > tr > th:nth-child(1) > div.sc-fLlhyt.bdchqW > div > div > span');
+    select_all.click();
+}, 100000);
 
-//Aperta o botão continuar. Aparece várias vezes no código
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 105000);
 
-//Aperta o listbox -   !! mesma situação de antes - mudança repetida de endereço do selector
-const list_open = document.querySelector('#downshift-94-toggle-button');
-list_open.click();
+setTimeout(() => {
+    const entrega_select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.iTOJzk > div:nth-child(1) > div.sc-bczRLJ.kJSGHS');
+    entrega_select.click();
+}, 110000);
 
-//Aperta o a opção 100 no listbox - sempre muda de endereço, e tem mesmo indíce do último !! mesma situação de antes
-const list100_select = document.querySelector('#downshift-94-item-4');
-list100_select.click();
+setTimeout(() => {
+    const continuar_button = document.querySelector('div.sc-bczRLJ.cllSmU > button');
+    continuar_button.click();
+}, 115000);
 
-//Seleciona todos os clientes da tela
-const Span_Select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-duzrYq.jtmVIG > table > thead > tr > th:nth-child(1) > div.sc-fLlhyt.bdchqW > div > div > span');
-Span_Select.click();
+setTimeout(() => {
+    const date_box = document.querySelector('#dueDate');
+    const selecionar_data = () => {
+        const hoje = new Date();
+        const selector_quatrosemanas = document.querySelectorAll('.DayPicker-Week .DayPicker-inner-day');
 
-//Seleciona o botão continuar, novamente
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+        for (const data of selector_quatrosemanas) {
+            const numeroDia = parseInt(data.textContent, 10);
 
-//Seleciona entrega grátis
-const entrega_select = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-gGnURB.iTOJzk > div:nth-child(1) > div.sc-bczRLJ.kJSGHS');
-entrega_select.click();
+            if (numeroDia > hoje.getDate()) {
+                data.click();
+                break;
+            }
+        }
+    };
+    date_box.click();
+    selecionar_data();
+}, 120000);
 
-//Seleciona o botão continuar, novamente
-const continuar_button = document.querySelector('#micro-frontend-crm > div.indexstyles__Root-partner-portal-crm__sc-3qo625-0.kXfgum > div.indexstyles__Root-partner-portal-crm__sc-kd480-0.cdQLVe.indexstyles__CampaignsPageBody-partner-portal-crm__sc-4bw979-0.iZidhd > div.sc-bczRLJ.cllSmU > button');
-continuar_button.click();
+setTimeout(() => {
+    const continuar2_button = document.querySelector(' div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 125000);
 
-//Clica no input data
-var clica_list = document.querySelector('#dueDate');
-clica_list.click();
+setTimeout(() => {
+    const gratisentregano_select = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-bZkfAO.eJQDwa > div > div > div:nth-child(5) > label');
+    gratisentregano_select.click();
+}, 130000);
 
-//Seleciona a data do dia de amanhã
-// Obter a data de hoje
-const hoje = new Date();
+setTimeout(() => {
+    const continuar2_button = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 135000);
 
-// Selecionar todos os elementos dentro de DayPicker-Week
-const selector_quatrosemanas = document.querySelectorAll('.DayPicker-Week .DayPicker-inner-day');
-
-// Iterar sobre os elementos para encontrar o primeiro número maior que o dia de hoje
-for (const data of selector_quatrosemanas) {
-    // Obter o número do dia do elemento e converter para número
-    const numeroDia = parseInt(data.textContent, 10);
-
-    // Verificar se o número do dia é maior que o dia de hoje
-    if (numeroDia > hoje.getDate()) {
-        // Simular o clique no elemento
-        data.click();
-        // Parar a iteração após o clique
-        break;
-    }
-}
-
-//Seleciona o botão continuar após a última ação
-const continuar2_button = document.querySelector('body > div:nth-child(32) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
-
-//Seleciona a opção - entrega grátis para quem segue
-const final1_select = document.querySelector('body > div:nth-child(32) > div.sc-llJcti.eqJNn > div > div.sc-bZkfAO.eJQDwa > div > div > div:nth-child(5) > label');
-final1_select.click();
-
-//Seleciona o botão continuar após a seleção, novamente
-const continuar2_button = document.querySelector('body > div:nth-child(32) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
-
-//Seleciona o botão continuar após a última verificação, com o mesmo endereço
-const continuar2_button = document.querySelector('body > div:nth-child(32) > div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
-continuar2_button.click();
+setTimeout(() => {
+    const continuar2_button = document.querySelector('div.sc-llJcti.eqJNn > div > div.sc-kLLXSd.iRtpfu > div > div:nth-child(2) > button.sc-kDDrLX.kIdHRO');
+    continuar2_button.click();
+}, 140000);
